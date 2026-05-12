3a42aabc2de22cf72b5627021dee807f989f3602
maryamsadiq17 <maryamsadiq5566@gmail.com>
2026-05-13 01:47:14 +0500

    Add variables declaration and logging to 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 1234567890
+let accountEmail ="john.doe@example.com"
+var accountPassword = "password123"
+accountCity = "New York"
+
+
+// accountId = 9876543210 // This will cause an error because accountId is a constant
+
+console.log("Account ID:", accountId)
