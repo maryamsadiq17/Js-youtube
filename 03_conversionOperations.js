83f126c88091f0d98f58aa4d23d9913b7685e5b9
maryamsadiq17 <maryamsadiq5566@gmail.com>
2026-05-13 19:47:56 +0500

    Add initial implementation of conversion operations and comparisons in 03_conversionOperations.js and 04_comaprisons.js

---
+++ 03_conversionOperations.js
@@ -0,0 +1,41 @@
+// let score = "Maryam"
+// let loggedIn = 5
+// let valueInNumber = Number (score)
+// let booleanLoggedIn = Boolean (loggedIn)
+// let age = String (18)
+// console.log(score)
+// console.log(typeof score, loggedIn)
+// console.table([ score, valueInNumber, loggedIn, age, booleanLoggedIn])
+
+
+
+// ****************Operations *************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2*2)
+// console.log(2**2);  //2's power
+
+
+let str1 = "Hello"
+let str2 = " Maryam"
+let str3 = str1 + str2
+console.log(  str3)
+
+
+// 
+
+// If there is string first them all code will be treated as string
+// But if there is not string first, the operations will be treated as usual 
+
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
---
+++ 04_comaprisons.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+
+
+// It will strictly chk the data types of both number
