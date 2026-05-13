3becc2a940c4c2715baf92ecc18c68fb158f7857
maryamsadiq17 <maryamsadiq5566@gmail.com>
2026-05-13 18:13:21 +0500

    Add initial implementation of data types and strict mode in 02_dataTypes.js

---
+++ 02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"       
+// for newer version of javascript, it is recommended to use strict mode to avoid common mistakes and bugs
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+ //  bad way to write the code, Deccreases the code readability
+console.log( 3 +
+    3)  
+
+// Data types in JavaScript
+
+let accountId = 17
+let accountName="Maryam"
+let isLoggedin = true
+let accountState;       //undefined
+
+// string ""
+// Numbers
+// Boolean
+// Symbols
+// Undefined   (undefined type)
+// Null    (object)  standalone value
+
+// symbol ( for uniqueness)
+
+
+console.log(typeof accountState);  //undefined
+
+// object
+
+console.log(typeof null); // object
